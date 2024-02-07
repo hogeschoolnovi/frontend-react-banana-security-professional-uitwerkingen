c6b0312620a988e78018f522bdabe825cc04f874
s-barnhoorn <sambarnhoorn@gmail.com>
2024-02-07 13:54:30 +0100

    main branch completed

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,48 @@
+import { useContext } from 'react';
+import logo from '../assets/banana-01.png';
+import { useNavigate, Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+
+function NavBar() {
+    const { isAuth, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    return (
+        <nav>
+            <Link to="/">
+          <span className="logo-container">
+            <img src={logo} alt="logo"/>
+            <h3>
+              Banana Security
+            </h3>
+          </span>
+            </Link>
+
+            {isAuth ?
+                <button
+                    type="button"
+                    onClick={logout}
+                >
+                    Log uit
+                </button>
+                :
+                <div>
+                    <button
+                        type="button"
+                        onClick={() => navigate('/signin')}
+                    >
+                        Log in
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => navigate('/signup')}
+                    >
+                        Registreren
+                    </button>
+                </div>
+            }
+        </nav>
+    );
+}
+
+export default NavBar;
